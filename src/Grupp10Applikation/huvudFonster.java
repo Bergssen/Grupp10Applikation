cb5cdf25946ca5b53913b21b0ef421c1cbcb8fee
aBeskow <47024539+aBeskow@users.noreply.github.com>
2019-02-05 11:36:33 +0100

    commit

--- src/Grupp10Applikation/huvudFonster.java
+++ src/Grupp10Applikation/huvudFonster.java
@@ -44,7 +44,6 @@ public class huvudFonster extends javax.swing.JFrame {
         initComponents();
 
         klickaAnvandarenamn();
-        
 
         lblValkomst.setText("Välkommen! Du är inloggad som användare.");
     }
